9dcee5ad9b4391ca024cd9f419880f7e1b84e589
Vishnu MM <vishnumm.2248@gmail.com>
2024-05-28 13:17:16 +0530

    Recovered changes for side-nav-bar and user-routing

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'post/:id', component: PostComponent },
+  { path: 'chat', component: PostComponent },
   {
     path: 'profile',
     children:[
